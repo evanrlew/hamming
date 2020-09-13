534919e4ae52240fb49320fdcc9e13ae1d792ddb
Evan Lew <erlew@calpoly.edu>
2020-09-12 22:49:19 -0500

    checkpoint, encoding seems to work. wont know until the decode works

--- hamming.py
+++ hamming.py
@@ -7,34 +7,81 @@ class Hamming:
         if not self._is_power_of_two(block_size):
             raise ValueError("block size={block_size} must be a power of two")
         self.block_size = block_size
-        self.block_overhead = math.log(self.block_size, 2) + 1
+        self.block_overhead = int(math.log(self.block_size, 2) + 1)
         self.block_capacity = self.block_size - self.block_overhead
 
     def encode(self, bytes_in, padding=0):
         # calculated overhead and output size
-        num_blocks = math.ceil(len(bytes_in) * 8 / block_capacity)
+        num_blocks = math.ceil(len(bytes_in) * 8 / self.block_capacity)
 
         # create input structure
-        bits_in_padded = np.full((num_blocks * block_capacity), padding, dtype=np.uint8)
-        bits_in_padded[0:len(bytes_in)] = np.unpackbits(bytes_in)
-        bits_in_packed = np.reshape(bytes_in_padded, (num_blocks, block_capacity))
+        bits_in_padded = np.full((num_blocks * self.block_capacity), padding, dtype=np.uint8)
+        bits_in_padded[0:len(bytes_in)*8] = np.unpackbits(bytes_in)
+        bits_in_packed = np.reshape(bits_in_padded, (num_blocks, self.block_capacity))
 
         # create output strcuture
-        bits_out_encoded = np.empty(num_blocks * self.block_size)
+        bits_out_encoded = np.empty(num_blocks * self.block_size, dtype=np.uint8)
 
-        for in_block in bytes_in_packed:
-            bits_out_encoded[?] = self._encode_block(in_block)
+        for ii, in_block in enumerate(bits_in_packed):
+            lsb = ii * self.block_size
+            msb = (ii+1) * self.block_size
+            bits_out_encoded[lsb:msb] = self._encode_block(in_block)
+
+        bytes_out_encoded = np.packbits(bits_out_encoded)
+        return bytes_out_encoded
 
 
 
     def _encode_block(self, in_block):
+        out_block = np.empty(self.block_size, dtype=np.uint8)
 
+        parity_locs = []
+        prev_loc = -1
+        for ii in reversed(range(self.block_overhead-1)):
+            loc = 2**ii + prev_loc
+            parity_locs.append( (loc, 2**ii) )
+            prev_loc = loc
+        parity_locs.append((parity_locs[-1][0]+1, 0))
+        in_block_idx = 0
         for ii in range(self.block_size):
+            if ii == parity_locs[0][0]:
+                _, mask_period = parity_locs.pop(0)
+                out_block[ii] = self._compute_parity_bit_using_mask(out_block[0:ii], mask_period)
+            else:
+                out_block[ii] = in_block[in_block_idx]
+                in_block_idx += 1
+
+        return out_block
+
+    def _compute_parity_bit_using_mask(self, in_block, mask_period):
+        mask_period_cntr = 0
+        counting_region = True
+        one_count = 0
+        for ii, bit in enumerate(in_block):
+            if counting_region:
+                one_count += bit
+
+            if mask_period == 0:
+                pass
+            if mask_period_cntr >= mask_period-1:
+                mask_period_cntr = 0
+                counting_region = not counting_region
+            else:
+              mask_period_cntr += 1
+        #print(f"{in_block} with mask_period = {mask_period} has one_count = {one_count & 1}")
+        return one_count & 1
+            
+
+
+            
+            
 
             
 
-    def decode(self, bits):
-        pass
+    def decode(self, bytes_in, padding=0):
+        bits_in = np.unpackbits(bytes_in)
+
+        
 
 
     def _is_power_of_two(self, val):
@@ -45,8 +92,16 @@ class Hamming:
 
 
 
-i             0 1 2 3 4 5 6 7 
-coverage            a   b c
-a             X X X X - - - -
-b             X X - - X X - -
-c             X - X - X - X -
+#   i             0 1 2 3 4 5 6 7 
+#   coverage            a   b c
+#   a             X X X X - - - -
+#   b             X X - - X X - -
+#   c             X - X - X - X -
+#   
+#   a = 2**2 - 1
+#   b = a + 2**1
+#   c = b + 2**0
+
+
+
+
--- test_hamming.py
+++ test_hamming.py
@@ -1,26 +1,45 @@
 from hamming import Hamming
+import numpy as np
 
-def test_hamming_simply_reconstruction():
-    ham = Hamming()
-    bits_ecc = ham.encode(bits)
-    bits_reconstructed, _ = ham.decode(bits_ecc)
+def test_hamming_single_block_encode_only():
+#   i             0 1 2 3 4 5 6 7 
+#   data = 11     1 0 1 - 1
+#   coverage            a   b c
+#   a             X X X S - - - -
+#   b             X X - - X S - -
+#   c             X - X - X - S -
+#   result        1 0 1 0 1 0 1 0
 
-    assert bits == bits_reconstructed
+    ham = Hamming(block_size=8)
+    in_block = np.unpackbits(np.array([0b1011], dtype=np.uint8)) #4'b1011
+    bits = ham._encode_block(in_block[4:8])
+    expected_bits = np.unpackbits(np.array([0b10101010], dtype=np.uint8))
+    assert (bits == expected_bits).all()
 
+def test_hamming_multiple_block_encode_only():
+    ham = Hamming(block_size=16)
+    in_block = bytearray(b"e")
+    bits = ham.encode(in_block)
+    assert len(bits) == 2
 
-def test_hamming_error_detection():
-    ham = Hamming()
-    bits_ecc = ham.encode(bits)
-    # corrupt bits
-    corruption_loc = None 
-    bad_bits = bits_ecc # FIXME
-    bits_reconstructed, errs = ham.decode(bad_bits)
-
-    assert bits == bits_reconstructed
-    assert errs == corruption_loc
-
-def test_hamming_double_error_detection():
-    pass
-
-
-
+#def test_hamming_simply_reconstruction():
+#    ham = Hamming()
+#    bits_ecc = ham.encode(bits)
+#    bits_reconstructed, _ = ham.decode(bits_ecc)
+#
+#    assert bits == bits_reconstructed
+#
+#
+#def test_hamming_error_detection():
+#    ham = Hamming()
+#    bits_ecc = ham.encode(bits)
+#    # corrupt bits
+#    corruption_loc = None 
+#    bad_bits = bits_ecc # FIXME
+#    bits_reconstructed, errs = ham.decode(bad_bits)
+#
+#    assert bits == bits_reconstructed
+#    assert errs == corruption_loc
+#
+#def test_hamming_double_error_detection():
+#    pass
